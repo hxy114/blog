4be23561c6e781ff4fcbfd9492b317d93a4851bd
1581882592 <56246181+1581882592@users.noreply.github.com>
2021-04-03 09:13:42 +0800

    Add files via upload

---
+++ blogBackEnd/HELP.md
@@ -0,0 +1,9 @@
+# release 1 login
+
+使用InteliJ IDEA开发（安装spring 插件）
+
+用InteliJ IDEA 打开后，等待maven自动解析依赖项
+
+运行程序后，使用浏览器访问localhost:8080/index  会自动跳转登录界面（用户名：admin  密码：123 可在templates文件夹的application.porperties中修改用户名和密码）
+
+进入后能看到简单的测试界面
---
+++ blogBackEnd/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index.html";
+    }
+}
+
